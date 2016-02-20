f70a774b62ea57763ba8c1ff8501bd6579a23c67
Ivan Rudoy <rudoy@farpost.com>
2016-02-20 11:22:30 +1000

    upd outdated packages; ref Gruntfile;

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,141 +1,135 @@
-var timer = require("grunt-timer");
+const timer = require('grunt-timer');
 
-module.exports = function(grunt) {
-timer.init(grunt);
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jade: {
-      compile: {
-        files: [{
-          cwd: 'source/jade',
-          src: ['**/*.jade', '!**/includes/*.jade'],
-          dest: 'dest',
-          expand: true,
-          ext: '.html'
-        }]
-      },
-      options: {
-        pretty: true,
-      }
-    },
-    sass: {
-        options: {
-            sourceMap: true,
-            outputStyle: 'compressed'
+module.exports = (grunt) => {
+    timer.init(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jade: {
+            compile: {
+                files: [{
+                    cwd: 'source/jade',
+                    src: ['**/*.jade', '!**/includes/*.jade'],
+                    dest: 'dest',
+                    expand: true,
+                    ext: '.html'
+                }]
+            },
+            options: {
+                pretty: true,
+            }
         },
-        dist: {
-            files: {
-                'source/css/app.css': 'source/sass/app.sass'
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'compressed'
+            },
+            dist: {
+                files: {
+                    'source/css/app.css': 'source/sass/app.sass'
+                }
+            }
+        },
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({
+                        browsers: ['> 2%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'Android >= 4', 'iOS >= 5', 'IE 10']
+                    })
+                ]
+            },
+            multiple_files: {
+                expand: true,
+                flatten: true,
+                src: 'source/css/*.css',
+                dest: 'dest/assets/stylesheets/'
+            }
+        },
+        copy: {
+            js: {
+                files: [{
+                    cwd: 'source/js',
+                    src: ['**/*.js'],
+                    dest: 'dest/assets/javascripts',
+                    expand: true,
+                }]
+            },
+            img: {
+                files: [{
+                    cwd: 'source/i',
+                    src: ['**/*'],
+                    dest: 'dest/assets/images',
+                    expand: true,
+                }]
+            },
+            fonts: {
+                files: [{
+                    cwd: 'source/fonts',
+                    src: ['**/*'],
+                    dest: 'dest/assets/fonts',
+                    expand: true,
+                }]
+            }
+        },
+        watch: {
+            js: {
+                files: ['source/js/**/*.js'],
+                tasks: ['copy:js'],
+                options: {
+                    livereload: true,
+                }
+            },
+            css: {
+                files: ['source/css/**/*.css'],
+                tasks: ['postcss'],
+                options: {
+                    livereload: true,
+                }
+            },
+            jade: {
+                files: ['source/**/*.jade'],
+                tasks: ['jade'],
+                options: {
+                    livereload: true,
+                }
+            },
+            sass: {
+                files: ['source/**/*.scss', 'source/**/*.sass' ],
+                tasks: ['sass']
+            },
+            img: {
+                files: 'source/i/**/*',
+                tasks: ['copy:img']
+            },
+            fonts: {
+                files: 'source/fonts/**/*',
+                tasks: ['copy:fonts']
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    hostname: '*',
+                    port: 3003,
+                    base: 'dest'
+                }
             }
         }
-    },
-    autoprefixer: {
-      options: {
-        browsers: [ '> 2%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'Android >= 4', 'iOS >= 5', 'IE 10' ]
-      },
-      multiple_files: {
-        expand: true,
-        flatten: true,
-        src: 'source/css/*.css',
-        dest: 'dest/assets/stylesheets/'
-      },
-    },
-    svg2png: {
-      all: {
-        files: [
-          {
-              src: ['source/i/**/*.svg']
-          }
-        ]
-      }
-    },
-    copy: {
-      js: {
-        files: [{
-          cwd: 'source/js',
-          src: ['**/*.js'],
-          dest: 'dest/assets/javascripts',
-          expand: true,
-        }]
-      },
-      img: {
-        files: [{
-          cwd: 'source/i',
-          src: ['**/*'],
-          dest: 'dest/assets/images',
-          expand: true,
-        }]
-      },
-      fonts: {
-        files: [{
-          cwd: 'source/fonts',
-          src: ['**/*'],
-          dest: 'dest/assets/fonts',
-          expand: true,
-        }]
-      }
-    },
-    watch: {
-      js: {
-        files: ['source/js/**/*.js'],
-        tasks: ['copy:js'],
-        options: {
-          livereload: true,
-        }
-      },
-      css: {
-        files: ['source/css/**/*.css'],
-        tasks: ['autoprefixer'],
-        options: {
-          livereload: true,
-        }
-      },
-      jade: {
-        files: ['source/**/*.jade'],
-        tasks: ['jade'],
-        options: {
-          livereload: true,
-        }
-      },
-      sass: {
-        files: ['source/**/*.scss', 'source/**/*.sass' ],
-        tasks: ['sass']
-      },
-      img: {
-        files: 'source/i/**/*',
-        tasks: ['copy:img']
-      },
-      fonts: {
-        files: 'source/fonts/**/*',
-        tasks: ['copy:fonts']
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          hostname: '*',
-          port: 3003,
-          base: 'dest'
-        }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  // grunt.loadNpmTasks('grunt-svg2png');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', [
-    'connect',
-    'copy',
-    'jade',
-    'sass',
-    'autoprefixer',
-    // 'svg2png',
-    'watch'
-  ]);
+    grunt.registerTask('default', [
+        'connect',
+        'copy',
+        'jade',
+        'sass',
+        'postcss',
+        'watch'
+    ]);
 }
