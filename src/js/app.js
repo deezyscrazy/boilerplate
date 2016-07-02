d415395c472f16372c3703bb9660d835a9d55cf4
Ivan Rudoy <rudooy@gmail.com>
2016-07-02 23:24:04 +1000

    upd deps

--- src/js/app.js
+++ src/js/app.js
@@ -3,46 +3,21 @@ import FastClick from 'fastclick';
 import is from 'is_js';
 
 $(() => {
-
+    /**
+     * DEV pagelist
+     */
     (($) => {
-        /**
-         ====================================================
-         DEV pagelist
-         ====================================================
-         **/
-
         let $debugPanel = $('.b-debug-pagelist');
 
         $debugPanel.find('.b-debug-pagelist__btn').on('click', () => {
             $debugPanel.toggleClass('active');
         });
-
-        // let clickTimer = null;
-        //
-        // $(document).on('touchstart', () => {
-        //     if (clickTimer === null)
-        //         clickTimer = setTimeout(() => { clickTimer = null; }, 200);
-        //     else {
-        //         clearTimeout(clickTimer);
-        //         clickTimer = null;
-        //         $('.b-debug-pagelist').fadeToggle(200);
-        //     }
-        // });
     })($);
-
 });
 
-FastClick.attach(document.body);
-
-/**
- ====================================================
- Touch :hover hack
- ====================================================
- **/
-
-if ( is.not.desktop() ) {
-    $('html').addClass('is-touch');
-    $('body').attr('ontouchstart', '');
-} else {
-    $('html').addClass('is-notouch');
-}
+(() => {
+    let className = is.not.desktop() ? 'is-touch' : 'is-notouch';
+    document.body.setAttribute('ontouchstart', '');
+    document.querySelector('html').classList.add(className);
+    FastClick.attach(document.body);
+})();
